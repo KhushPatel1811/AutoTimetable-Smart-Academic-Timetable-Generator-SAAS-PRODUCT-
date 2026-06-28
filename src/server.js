a90e2a797e46d91a1d35ca1b58d4ad5fdd48909c
KhushPatel1805 <khush5237@gmail.com>
2026-06-28 13:57:34 +0530

    This is the 19th commit in which just updated the cors portion in server.js and added images to the screenshot folder in public folder.

--- src/server.js
+++ src/server.js
@@ -13,9 +13,13 @@ import timeTableRoutes from './Routes/timetable.route.js'
 dotenv.config({path: '../.env'});    
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: "https://smart-academic-timetable-generator.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 const PORT = process.env.PORT || 2000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/Institute_Time_Table";
